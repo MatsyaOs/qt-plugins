aa828049854f7927adb928b77013787ba173aa48
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-16 07:07:57 +0530

    matyou

---
+++ build/platformtheme/CMakeFiles/matsyaplatformtheme.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matsyaplatformtheme_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/matsyaplatformtheme_autogen.dir/ParseCache.txt"
+  "matsyaplatformtheme_autogen"
+  "CMakeFiles/matsyaplatformtheme.dir/hintsettings.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/main.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/matsyaplatformtheme_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/platformtheme.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/qdbusmenubar.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/statusnotifier/dbustypes.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/statusnotifier/statusnotifieritem.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/statusnotifieritemadaptor.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/statusnotifierwatcher_interface.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/systemtrayicon.cpp.o"
+  "CMakeFiles/matsyaplatformtheme.dir/x11integration.cpp.o"
+  "libmatsyaplatformtheme.pdb"
+  "libmatsyaplatformtheme.so"
+  "statusnotifieritemadaptor.cpp"
+  "statusnotifieritemadaptor.h"
+  "statusnotifieritemadaptor.moc"
+  "statusnotifierwatcher_interface.cpp"
+  "statusnotifierwatcher_interface.h"
+  "statusnotifierwatcher_interface.moc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matsyaplatformtheme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
